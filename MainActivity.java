7047a0fffded2956925a52fef372c27b8928ba41
Nathy10Carvalh0 <134658039+Nathy10Carvalh0@users.noreply.github.com>
2023-05-25 22:39:01 -0300

    app cadastro
    o app lê e mostra as informações digitadas pelo usuário.

--- MainActivity.java
+++ MainActivity.java
@@ -1,53 +1,33 @@
-    package com.example.mediaaluno;
+package com.example.app_cadastro;
 
-    import static android.app.ProgressDialog.show;
+import androidx.appcompat.app.AppCompatActivity;
 
-    import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-    import android.os.Bundle;
-    import android.view.View;
-    import android.widget.EditText;
-    import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
 
-    public class MainActivity extends AppCompatActivity {
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-        }
-        EditText nomeAluno, nota01, nota02, nota03, nota04;
-
-        public void calcular (View v) {
-            //capturando valores digitados na Activity
-            nomeAluno = (EditText) findViewById(R.id.txt_nome_aluno);
-            nota01 = (EditText) findViewById(R.id.txt_nota01);
-            nota02 = (EditText) findViewById(R.id.txt_nota02);
-            nota03 = (EditText) findViewById(R.id.txt_nota03);
-            nota04 = (EditText) findViewById(R.id.txt_nota04);
-
-            double media_aluno = (
-                    Double.parseDouble(nota01.getText().toString()) +
-                            Double.parseDouble(nota02.getText().toString()) +
-                            Double.parseDouble(nota03.getText().toString()) +
-                            Double.parseDouble(nota04.getText().toString())
-                    )/4;
-            //if no java
-            String texto_comparacao = null;
-            if (media_aluno >= 6) {
-                texto_comparacao = "Aprovado";
-            } else if (media_aluno <6) {
-                texto_comparacao = " Reprovado";
-            }
-
-
-            // mostrando resultados no Toast no android
-            Toast.makeText(getApplicationContext(),
-                    "Nome aluno: " + nomeAluno.getText() +
-                            "\nMedia aluno: " + media_aluno +
-                            "\nSituação do aluno: " + texto_comparacao,
-                    Toast.LENGTH_LONG).show();
-
-
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
+    EditText nome_dg, celular_dg, email_dg;
+
+    public void dados_cadastrados(View v)
+    {
+        //capturando os valores do EditText
+        nome_dg = (EditText) findViewById(R.id.txt_nome);
+        celular_dg = (EditText) findViewById(R.id.txt_celular);
+        email_dg = (EditText) findViewById(R.id.txt_email);
+
+        Toast.makeText(getApplicationContext(),
+                "nome: "+ nome_dg.getText()+ "\n" +
+                "celular: "+ celular_dg.getText()+ "\n" +
+                "E-mail: "+ email_dg.getText(),
+
+                Toast.LENGTH_LONG).show();
+    }
+}
