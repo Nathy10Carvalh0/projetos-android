fd7e74fd896ace476d1119fd4e2207ad89e9f3e4
Nathalia Carvalho <134658039+Nathy10Carvalh0@users.noreply.github.com>
2023-09-04 12:36:40 -0300

    Add files via upload

--- APP_Calculos/MainActivity.java
+++ APP_Calculos/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app_cadastro;
+package com.example.app_calculos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,20 +15,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    EditText nome_dg, celular_dg, email_dg;
+    EditText numero01, numero02;
 
-    public void dados_cadastrados(View v)
-    {
-        //capturando os valores do EditText
-        nome_dg = (EditText) findViewById(R.id.txt_nome);
-        celular_dg = (EditText) findViewById(R.id.txt_celular);
-        email_dg = (EditText) findViewById(R.id.txt_email);
+    public void calculos (View v) {
+        //capturando valores digitados na Activity
+        numero01 = (EditText) findViewById(R.id.txt_numero01);
+        numero02 = (EditText) findViewById(R.id.text_numero02);
 
+        int res_soma =
+                Integer.parseInt(numero01.getText().toString()) +
+                        Integer.parseInt(numero02.getText().toString());
+        Double res_div = (
+                Double.parseDouble(numero01.getText().toString()) +
+                        Double.parseDouble(numero02.getText().toString())
+        ) / 2;
+
+            //if no java
+        int brasil = Integer.parseInt(numero01.getText().toString());
+        int argentina = Integer.parseInt(numero02.getText().toString());
+
+        String texto_comparacao = null;
+        if (brasil > argentina) {
+            texto_comparacao = "brasil > argentina";
+        } else if (brasil < argentina) {
+            texto_comparacao = " brasil < argentina";
+        } else {
+            texto_comparacao = "brasil = argentina";
+        }
+
+
+        // mostrando resultados no Toast no android
         Toast.makeText(getApplicationContext(),
-                "nome: "+ nome_dg.getText()+ "\n" +
-                "celular: "+ celular_dg.getText()+ "\n" +
-                "E-mail: "+ email_dg.getText(),
-
+                "resultado da soma: " + res_soma +
+                        "\nResultado da divisão:" + res_div +
+                        "\nComparação de Variáveis: " + texto_comparacao,
                 Toast.LENGTH_LONG).show();
     }
-}
+    }
